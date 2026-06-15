42a3ee02281d2c7458db974b2a6814b6c2f3ab0e
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-06-15 04:55:21 -0500

    more intro message.

--- src/BROS.Processing/Initializers/WorldInitializer.vb
+++ src/BROS.Processing/Initializers/WorldInitializer.vb
@@ -6,6 +6,7 @@ Friend Module WorldInitializer
         Dim frontYard = world.CreateLocation(AddressOf FrontYardInitializer.Initialize)
         world.CreateLocation(BluerRoomInitializer.Initialize(frontYard))
         world.AddMessage("Welcome to The Bluer Room of SPLORR!!")
+        world.AddMessage("There is a HELP command. You should use it. Unless yer THAT guy.")
         world.Avatar.DescribeLocation()
     End Sub
 End Module
