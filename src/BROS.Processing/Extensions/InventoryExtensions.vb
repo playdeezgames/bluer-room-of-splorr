4ed58c8adcb69c8da2d71dcb55097135166df229
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-06-22 05:06:50 -0500

    give stub is there.

---
+++ src/BROS.Processing/Extensions/InventoryExtensions.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Runtime.CompilerServices
+Imports BROS.Persistence
+
+Friend Module InventoryExtensions
+    <Extension>
+    Friend Function CanAccept(inventory As IInventory, item As IItem) As Boolean
+        Return False
+    End Function
+End Module
--- src/BROS.Processing/Processors/Processors.vb
+++ src/BROS.Processing/Processors/Processors.vb
@@ -17,7 +17,8 @@ Friend Module Processors
             ("unlock", {"Attempts to unlock a route in a particular direction with a particular item.", "Example:", "    UNLOCK [DIRECTION] WITH [ITEM]"}, AddressOf UnlockCommandProcessor.Process),
             ("talk", {"Attempts communication with another character.", "Example:", "    TALK TO [CHARACTER]"}, AddressOf TalkCommandProcessor.Process),
             ("ask", {"Posits a query about a selected motif to an interlocutor. If you don't know what to ask about, ask about `TOPICS`.", "Example:", "    ASK [CHARACTER] ABOUT [TOPIC]", "    ASK [CHARACTER] ABOUT TOPICS"}, AddressOf AskCommandProcessor.Process),
-            ("equip", {"Attempts to equip an item.", "Example:", "    EQUIP [ITEM] [PREPOSITION] [EQUIP-SLOT]"}, AddressOf EquipCommandProcessor.Process)
+            ("equip", {"Attempts to equip an item.", "Example:", "    EQUIP [ITEM] [PREPOSITION] [EQUIP-SLOT]"}, AddressOf EquipCommandProcessor.Process),
+            ("give", {"Attempts to give an item to another character.", "Example:", "    GIVE [ITEM] TO [CHARACTER]"}, AddressOf GiveCommandProcessor.Process)
         }
     Private ReadOnly processorTable As IReadOnlyDictionary(Of String, Func(Of IWorld, Queue(Of String), CommandProcessorResult)) =
         commandList.ToDictionary(
